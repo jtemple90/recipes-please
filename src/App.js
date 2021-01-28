ea9cbb00e5a38b393a0c57b41f9f83d031e8426d
John Temple <johntemple@Alenas-iMac.local>
2021-01-27 21:22:30 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Recipe from './components/recipe/Recipe'
 
-function App() {
+const App = () => {
+  const APP_ID = 'efe15b82';
+  const APP_KEY = 'beb69207452cb99df452362e8d2a1a73';
+
+  const [recipes, setRecipes] = useState([]);
+  
+
+  useEffect(() => {
+    getRecipes()
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits)
+
+    // fetch(
+    //   `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    // ).then(response => {
+    //   response.json()
+    // })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Recipes Please</h1>
+      <form className='search-form'>
+        <input className='search-bar' type="text"/>
+        <button className='search-btn' type='submit'>Search</button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe />
+      ))}
     </div>
-  );
-}
+  )
+} 
 
 export default App;
---
+++ src/components/recipe/Recipe.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Recipe.css'
+
+const Recipe = () => {
+  return (
+    <div className="">
+      <h1>Title</h1>
+      <p>Calories</p>
+      <img src="" alt=""/>
+    </div>
+  )
+}
+ export default Recipe
